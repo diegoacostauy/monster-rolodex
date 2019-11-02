c37643b5c28764bc591ec82e282d0e0f6d857a2c
diegaco <diegoacosta211@gmail.com>
2019-11-02 20:02:59 -0300

    Updates

---
+++ precache-manifest.5576e73ba4ef3d9bb6d2256157361431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb43a162a7e95d174a1f1567132bc91",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4992b17f6cb93d55e5d8",
+    "url": "/monster-rolodex/static/css/main.b9dc2d3b.chunk.css"
+  },
+  {
+    "revision": "a20f2964f1e7103d046d",
+    "url": "/monster-rolodex/static/js/2.3860c490.chunk.js"
+  },
+  {
+    "revision": "4992b17f6cb93d55e5d8",
+    "url": "/monster-rolodex/static/js/main.67fd2f70.chunk.js"
+  },
+  {
+    "revision": "f75c77ac67bebe39c115",
+    "url": "/monster-rolodex/static/js/runtime-main.749f14a7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monster-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
